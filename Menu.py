2c26a1825f2c237192487317ec80d660cfc93708
xsamluvs <sam05.1415@hotmail.com>
2025-05-13 16:20:18 -0600

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -20,19 +20,19 @@ pygame.int()
 screen = pygame.display.set_mode((1000,800))
 
 # fondo de pantalla
-background = pygame.image.load('/home/pc04/juego progra/fondomar.png')
+background = pygame.image.load('fondo.png´)
 
 # sonido de fondo
-mixer.music.load('/home/pc04/juego progra/UTF-8background.wav')
+mixer.music.load('UTF-8background.wav')
 mixer.music.play(-1)
 
 # titulo y icono
 pygame.display.set_caption("Perdidos en el mar")
-icon = pygame.image.load('/home/pc04/juego progra/orcadepredadora.png')
+icon = pygame.image.load('orcamala1.png')
 pygame.display.set_icon(icon)
 
 # jugador
-PlayerImg=pygame.image.load('/home/pc04/juego progra/nutria1.png')
+PlayerImg=pygame.image.load('nutria1.png')
 playerX=370
 playery=480
 playerX_change=0
@@ -46,14 +46,14 @@ enemyY_change=[]
 num_of_enemies= 20
 
 for i in range(num_of_enemies):
-    enemyImg.append(pygame.image.load('/home/pc04/juego progra/tiburondepredador1.png'))
+    enemyImg.append(pygame.image.load('tiburondepredador1.png'))
     enemyX.append(random.randint(0,736))
     enemyY.append(random.randint(20,50))
     enemyX_change.append(4)
     enemyY_change.append(40)
 
 # tridente
-armaImg=pygame.image.load('/home/pc04/juego progra/tridente1.png')
+armaImg=pygame.image.load('tridente.png')
 armaX=0
 armaY=480
 armaX_change=0
@@ -64,14 +64,14 @@ arma_estado='ready'
 
 
 score_value=0
-font=pygame.font.Font('/home/pc04/juego progra/sakitu_baelah_clean.zip',32)
+font=pygame.font.Font('sakitu_baelah_clean.zip',32)
 
 
 textX=10
 textY=10
 
 # juego terminado
-over_font=pygame.font.Font('/home/pc04/juego progra/super_crumble.zip',64)
+over_font=pygame.font.Font('super_crumble.zip',64)
 
 def show_puntaje(x,y):
     score=font.render("Score:"+str(score_value).true,(255,255,255))
@@ -110,5 +110,19 @@ while running:
                      playerX_change=-5
                   if event.key==pygame.K_RIGHT:
                      playerX_change= 5
+
+#RGB = Red, Green, Blue
+screen.fill((0,0,0))
+#Background Image
+screen.blit(background,(0,0))
+for event in pygame.event.get():
+    if event.type = pygame.QUIT
+         running = False
+
+# if keystroke is pressed check whether its right or left
+if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            playerX_change = -5
+
 
                                  
